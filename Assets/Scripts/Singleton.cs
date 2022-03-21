31d08a52f5ecaf1da7f40e73fca3fdd3d1c716ba
Daniel Cumbor <dcdolphy@gmail.com>
2022-03-21 10:11:35 +0000

    Created Singleton class.

--- Assets/Scripts/Singleton.cs
+++ Assets/Scripts/Singleton.cs
@@ -1,18 +1,40 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
-public class Singleton : MonoBehaviour
+public class Singleton<T> : MonoBehaviour where T : Component
 {
-    // Start is called before the first frame update
-    void Start()
+    public static T Instance
     {
-        
+        get
+        {
+            if (_instance != null) return _instance;
+
+            _instance = FindObjectOfType<T>();
+            
+            if (_instance != null) return _instance;
+
+            var gameObject = new GameObject
+            {
+                name = typeof(T).Name
+            };
+            _instance = gameObject.AddComponent<T>();
+
+            return _instance;
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    private static T _instance;
+
+    public virtual void Awake()
     {
-        
+        if (_instance == null)
+        {
+            _instance = this as T;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            // Must be a duplicate, destroy it.
+            Destroy(gameObject);
+        }
     }
 }
